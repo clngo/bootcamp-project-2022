616e7a83a5e5ba9ca9efe33adf2c463b8b6ce49e
Colin Ngo <clngo654@gmail.com>
2024-10-17 18:56:29 -0700

    list of blog

---
+++ src/blog.ts
@@ -0,0 +1,27 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+}
+
+const blogs: Blog[] = [
+    {
+        title: "Project1",
+        date: "10/17/2024",
+        description: "Project Description",
+        image: "Project Image",
+        imageAlt: "ImageAlt",
+        slug: "hyperlink"
+    },
+    {
+        title: "Project2",
+        date: "10/17/2024",
+        description: "Project Description",
+        image: "Project Image",
+        imageAlt: "ImageAlt",
+        slug: "hyperlink"
+    }
+]
